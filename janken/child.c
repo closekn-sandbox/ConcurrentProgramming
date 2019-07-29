69c1002b38dda26370069e98934bfe4158c7fd23
closekn <close.me.kn@gmail.com>
2019-07-29 15:00:00 +0900

    get socketjanken

--- janken/child.c
+++ janken/child.c
@@ -9,13 +9,80 @@
 #include <netdb.h>
 
 #define	SERV_TCP_PORT 20000 // port_no
+#define SLEEP_TIME    4000  // ms
+#define BYTE          100   // msg byte
+#define IDENT_BYTE    35    // ident byte
+
+//-- func
+void err_msg(char *msg);
 
 int main(int argc, char *argv[]) {
+  char *sname;                 // argv[1] server name
+  char identifier[IDENT_BYTE]; // argv[2] identifier
+  struct hostent *retrieve;
+  struct sockaddr_in serv_addr;
+  int ipaddr;                  // server IP
+  int port_no;                 // port number
+  int sockfd;                  // socket
+  // flag
+  int join;
 
-  // janken
-  while ( 1 ) {
-
+  // 引数不足 > ./child <server-name> <identifier>
+  if ( argc < 3 ) {
+    fprintf(stderr, "Usage : %s <server_name> <identifier>\n", argv[0]);
+    exit(1);
   }
 
+  // サーバ
+  sname = argv[1];
+  // サーバ見つからない場合エラー1
+  if ((retrieve = gethostbyname(sname)) == NULL) {
+    fprintf(stderr, "Unknown host name: %s\n", sname);
+    exit(1);
+  }
+  // サーバIP取得 ポート取得
+  ipaddr = *(unsigned int *)(retrieve->h_addr_list[0]);
+  port_no = SERV_TCP_PORT;
+  // ローカルIP取得
+  bzero((char *)&serv_addr, sizeof(serv_addr));
+  serv_addr.sin_family = AF_INET;
+  serv_addr.sin_addr.s_addr = *(unsigned int *)(retrieve->h_addr_list[0]);
+  serv_addr.sin_port = htons(port_no);
+
+  // 識別子
+  sprintf(identifier, "%s", argv[2]);
+  if ( strlen(identifier) > IDENT_BYTE ) {
+    fprintf(stderr, "Over identifier size: %s\n", identifier);
+    exit(1);
+  }
+
+  // ソケットの作成(TCP接続)
+  if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+    err_msg("client: can't open datastream socket");
+  }
+    
+  // 能動オープン(コネクション要求)
+  if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+    err_msg("client: can't connect server address");
+  }
+  fcntl(fileno(stdin), F_SETFL, O_NONBLOCK);
+  fcntl(fileno(stdout), F_SETFL, O_NONBLOCK);
+  fcntl(sockfd, F_SETFL, O_NONBLOCK);
+
+  //-- janken
+  while ( 1 ) {
+    
+    break;//
+  }
+
+  // end
+  close(sockfd);
+
   return 0;
+}
+
+// エラーメッセージ
+void err_msg(char *msg) {
+  perror(msg);
+  exit(1);
 }
--- janken/parent.c
+++ janken/parent.c
@@ -11,9 +11,73 @@
 #include <netdb.h>
 
 #define	SERV_TCP_PORT 20000 // port_no
+#define BYTE          100   // msg byte
+#define MAX_CHNUM     2
 
+//-- func
+void err_msg(char *msg);
+
+//-- main
 int main(int argc, char *argv[]) {
+  struct sockaddr_in serv_addr, cli_addr;
+  socklen_t cli_len = sizeof(cli_addr);
+  int port_no;            // ポート番号
+  int sockid;             // コネクション待ちソケット
+  int sockfd[MAX_CHNUM];  // 通信用ソケット
+  // user
+  int chnum = 0;          // child num
 
+  // rep
+  int i;
+  
+
+  // 引数不足
+
+  // ログファイル指定
+
+  // コネクション要求受付用のソケット確保
+  if ((sockid = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+    err_msg("server: can't open datastream socket");
+  }
+  // ソケットとポートの対応づけ
+  port_no = SERV_TCP_PORT;
+  bzero((char *) &serv_addr, sizeof(serv_addr));
+  serv_addr.sin_family	  = AF_INET;
+  serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+  serv_addr.sin_port	  = htons(port_no);
+  if (bind(sockid, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+    err_msg("server: can't bind local address");
+  }
+  // 受動オープン状態へ移行
+  if (listen(sockid, 5) == -1) {
+    err_msg("server: listen failed");
+  }
+
+  //-- janken
+  while ( 1 ) {
+    while ( chnum < MAX_CHNUM ) {
+      fprintf(stderr ,"あと%d名\n", MAX_CHNUM-chnum);
+      // クライアントからコネクションの要求を受け入れる 
+      if ((sockfd[chnum] = accept(sockid, (struct sockaddr *)&cli_addr, &cli_len)) < 0) {
+        close(sockid);
+        fprintf(stderr, "Server: can't accept");
+        break;
+      }
+      chnum++;
+    }
+    
+    break;  //
+  }
+
+  // end
+  close(sockid);
+  for ( int i = 0; i < MAX_CHNUM; i++ ) { close(sockfd[i]); }
 
   return 0;
+}
+
+// エラーメッセージ
+void err_msg(char *msg) {
+  perror(msg);
+  exit(1);
 }
